74b0cd229a2352c64601b251b165bf9dfd8241f6
elukuro <heruhartantot@gmail.com>
2022-01-09 11:33:45 +0700

    section 12 134 create callback inside context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,25 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 // create context
 const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
   // provider are source information that avaliable for child component initialize before app component
-  // it's bring value that able to pass around the child component s
+  // it's bring value that able to pass around the child component
+
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPosts = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog post # ${blogPosts.length + 1}` },
+    ]);
+  };
   return (
-    <BlogContext.Provider value={"Hi there "}>{children}</BlogContext.Provider>
+    <BlogContext.Provider
+      value={{ data: blogPosts, addBlogPost: addBlogPosts }}
+    >
+      {children}
+    </BlogContext.Provider>
   );
 };
 
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,13 +1,22 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
   // get value from BlogProvider using useContext function from react
-  const value = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
+
+  // section 12 134 context is the way to moving information not to manage state
+  // managing state are using useState
   return (
     <View>
-      <Text>test {value}</Text>
+      <Text>Index Screen</Text>
+      <Button title="Add Posts" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </View>
   );
 };
