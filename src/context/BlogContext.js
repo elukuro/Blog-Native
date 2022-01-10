e30ec153101dae18c3d7fa5536aca2e509e615ec
elukuro <heruhartantot@gmail.com>
2022-01-10 07:05:40 +0700

    section 12 136 Initialize reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,22 +1,44 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 // create context
 const BlogContext = createContext();
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_BLOGPOST":
+      return [...state, { title: `Blog post # ${state.length + 1}` }];
+    case "DELETE_BLOGPOST":
+      return [...state];
+    default:
+      return state;
+  }
+};
 
 export const BlogProvider = ({ children }) => {
   // provider are source information that avaliable for child component initialize before app component
   // it's bring value that able to pass around the child component
 
-  const [blogPosts, setBlogPosts] = useState([]);
+  // const [blogPosts, setBlogPosts] = useState([]);
 
-  const addBlogPosts = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog post # ${blogPosts.length + 1}` },
-    ]);
+  // initialize reducer
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  // const addBlogPosts = () => {
+  //   setBlogPosts([
+  //     ...blogPosts,
+  //     { title: `Blog post # ${blogPosts.length + 1}` },
+  //   ]);
+  // };
+
+  const addBlogPost = () => {
+    dispatch({ type: "ADD_BLOGPOST" });
   };
+
   return (
     <BlogContext.Provider
-      value={{ data: blogPosts, addBlogPost: addBlogPosts }}
+      value={{
+        data: blogPosts,
+        addBlogPost,
+      }}
     >
       {children}
     </BlogContext.Provider>
